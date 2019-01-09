68d44ab861637409fbcc60d4f66c6c7870019f92
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-09 20:14:23 +0200

    another test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'Hi There!' });
+    res.send({ hi: 'Hi There!', q: 'this is another test!' });
 });
 
 const PORT = process.env.PORT;
