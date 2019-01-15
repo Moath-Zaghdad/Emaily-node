72788ccba24e999317a251fc27eefe43d3bc6c93
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-15 23:54:20 +0200

    chagen the mongo URL to be env

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: `mongodb://${process.env.DB_USER}:${
-        process.env.DB_PASSWORD
-    }@ds255754.mlab.com:55754/emaily-dev-db`,
+    mongoURI: process.env.MONGO_URL,
     cookieKey: process.env.COOKIE_KEY,
 };
